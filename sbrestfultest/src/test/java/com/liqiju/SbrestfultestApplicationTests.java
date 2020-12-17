0188406c07e3f8752c12f3a04bb13da1f9ab0c04
liqiju <helloliqiju@gmail.com>
2020-12-17 17:48:58 +0800

    init commit

---
+++ sbrestfultest/src/test/java/com/liqiju/SbrestfultestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liqiju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbrestfultestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
